e946deb15f5d4145a1b6d26da7e93076bdfac627
dmrudy17 <dmr136@zips.uakron.edu>
2020-12-12 18:30:54 -0500

    Add pipeline script with additional features and report

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,35 +1,26 @@
 pipeline {
-    agent {
-        docker {
-            image 'drudy/ubuntu20.04'
-            args '--user root'
-        }
-    }
+    agent any
 
     stages {
-        stage('Setup') {
-            steps {
-                git branch: 'main', credentialsId: 'GitHub', url: 'https://github.com/UAComputerScience/markturn-dmrudy17.git'
+        stage('Build Package') {
+            agent {
+                docker {
+                    image 'drudy/markturn_ubuntu20.04'
+                    args '--user root'
+                }
             }
-        }
-        stage('CMake') {
+
             steps {
-                cmakeBuild buildDir: 'build', cmakeArgs: '-D LINK_STATIC=OFF', generator: 'Ninja', installation: 'cmake'
-            }
-        }
-        stage('Build Generation') {
-            steps {
-                sh 'cd build; ninja'
-            }
-        }
-        stage('Test') {
-            steps {
-                ctest installation: 'cmake', workingDir: 'build'
-            }
-        }
-        stage('Package') {
-            steps {
-                cpack arguments: '-G DEB', installation: 'cmake', workingDir: 'build'
+                sh 'cat /etc/os-release'
+                sh 'curl -L https://github.com/UAComputerScience/devops-f20-final-exam-dmrudy17/archive/v1.0.0.tar.gz | tar xz'
+                sh 'mv devops-f20-final-exam-dmrudy17-1.0.0 /Source'
+                sh 'mkdir /Build'
+                sh 'cd /Build; cmake /Source -DLINK_STATIC=OFF -G Ninja'
+                sh 'cd /Build; ninja'
+                sh 'cd /Build; ctest'
+                sh 'cd /Build; cpack -G DEB'
+                sh 'cp /Build/*.deb dist/.'
+                archiveArtifacts artifacts: 'dist/*.deb', followSymlinks: false
             }
         }
     }
